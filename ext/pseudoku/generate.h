ef2ffe967da2c63b8bb9e6ebf13f8b6ffb17603c
Pete Bevin <pete@hover.com>
2014-05-13 16:09:04 -0400

    Use C style comment in generate.h

--- ext/pseudoku/generate.h
+++ ext/pseudoku/generate.h
@@ -1,3 +1,5 @@
-// Generate a Sudoku puzzle and place it into
-// output, which must have 81 characters available.
+/*
+ * Generate a Sudoku puzzle and place it into
+ * output, which must have 81 characters available.
+ */
 extern void generate(char *output);
