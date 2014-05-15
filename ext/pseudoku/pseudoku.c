c265aa99c19fbabfa82dcd8baf3838bdbfd9c42e
Pete Bevin <pete@hover.com>
2014-05-15 14:53:30 -0400

    Speed up Solver by only initializing once

--- ext/pseudoku/pseudoku.c
+++ ext/pseudoku/pseudoku.c
@@ -5,22 +5,21 @@
 
 int debug = 0;
 
-static VALUE ps_generate(VALUE self) {
-  char grid[82];
-  generate(grid);
-  return rb_str_new2(grid);
+static VALUE ps_alloc(VALUE solverClass) {
+  struct solver *solver = malloc(sizeof(struct solver));
+  memset(solver, 0, sizeof(*solver));
+  initsolve(solver);
+  return Data_Wrap_Struct(solverClass, NULL, free, solver);
 }
-
-struct solver solver;
 
 static VALUE ps_solve(VALUE self, VALUE pattern) {
   int puzzle[81];
   char grid[82];
   int i;
   const char *arg = StringValueCStr(pattern);
+  struct solver *solver;
 
-  memset(&solver, 0, sizeof(solver));
-  initsolve(&solver);
+  Data_Get_Struct(self, struct solver, solver);
 
   for (i = 0; i < 81; i++) {
     if (isdigit(arg[i])) {
@@ -34,14 +33,11 @@ static VALUE ps_solve(VALUE self, VALUE pattern) {
   //  0: no solution found
   //  1: unique solution found
   //  2: multiple solutions found
-  if (debug) printf("backtracks before: %d\n", solver.solver_backtracks);
-  int rc = solve(&solver, puzzle);
-
-  if (debug) printf("backtracks after: %d\n", solver.solver_backtracks);
+  int rc = solve(solver, puzzle);
 
   if (rc == 1) {
     for (i = 0; i < 81; i++) {
-      grid[i] = solver.solution[i] + '0';
+      grid[i] = solver->solution[i] + '0';
     }
 
     return rb_str_new(grid, 81);
@@ -63,13 +59,23 @@ static VALUE ps_nodebug(VALUE self) {
 }
 
 static VALUE ps_backtracks(VALUE self) {
-  return INT2NUM(solver.solver_backtracks);
+  struct solver *solver;
+
+  Data_Get_Struct(self, struct solver, solver);
+  return INT2NUM(solver->solver_backtracks);
+}
+
+static VALUE ps_generate(VALUE self) {
+  char grid[82];
+  generate(grid);
+  return rb_str_new2(grid);
 }
 
 void Init_pseudoku() {
   VALUE pseudoku = rb_define_module("Pseudoku");
 
   VALUE solver = rb_define_class_under(pseudoku, "Solver", rb_cObject);
+  rb_define_alloc_func(solver, ps_alloc);
   rb_define_method(solver, "generate", ps_generate, 0);
   rb_define_method(solver, "solve", ps_solve, 1);
   rb_define_method(solver, "backtracks", ps_backtracks, 0);
